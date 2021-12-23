ca4986822eea5abc133bba33475376c47e52ee7b
Isabella Damião <isabella.damiao@wconnect.com.br>
2021-12-23 16:38:47 -0300

    up environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   //baseUrl: 'http://localhost:8080/'
-  baseUrl: 'http://calm-hollows-73101.herokuapp.com/agenda'
+  baseUrl: 'http://calm-hollows-73101.herokuapp.com/agenda/'
 };
 
 /*
